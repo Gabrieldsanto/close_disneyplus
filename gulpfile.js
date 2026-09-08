b70e3c4bbc89da22a5e0adcaff415b2b7228e3a0
gabriel <gabriel@teste.com>
2026-09-07 23:30:07 -0300

    Ajustes de layout no Hero, FAQ e correções no Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 }
 
-function styles(){
-    return gulp.src ('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-
-function images(){
-    return gulp.src ('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+function images() {
+    return gulp.src('./src/images/**/*', { encoding: false }) // { encoding: false } impede que o Gulp corrompa imagens binárias
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
-
 
 exports.default = gulp.parallel(styles, images, scripts);
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+
+exports.watch = function() {
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles)); // Adicionado **/*.scss para monitorar subpastas do Sass
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Adicionado monitoramento das imagens
 }
